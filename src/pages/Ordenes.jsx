d852d4bdedbcb46181f0a158aae8951cc4a71554
Ana Laura Flores <anadisenio@hotmail.com>
2025-12-21 18:55:09 -0300

    modify ordenes orden.comprobante.numero

--- src/pages/Ordenes.jsx
+++ src/pages/Ordenes.jsx
@@ -106,7 +106,7 @@ export default function Ordenes() {
                                 </td>
 
                                 <td className="p-2">
-                                    {orden.comprobante[0].numero || "-"}
+                                    {orden.comprobante.numero || "-"}
                                 </td>
 
                                 <td className="p-2">
