9fce6f28a18cde9ed54d0e63e86251283434a132
Ana Laura Flores <anadisenio@hotmail.com>
2025-12-22 16:44:08 -0300

    mainlayoutgris

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,16 +1,12 @@
-import SidebarDesktop from "../components/Sidebar";
+import Sidebar from "../components/Sidebar";
 import BottomNav from "../components/BotoomNav";
 
 export default function MainLayout({ children }) {
   return (
-    <div className="flex min-h-screen">
-      <SidebarDesktop />
-
-      <main className="flex-1 p-4 pb-20 md:pb-4">
-        {children}
-      </main>
-
-      <BottomNav />
+    <div className="flex bg-gray-900 text-gray-100 min-h-screen">
+      <Sidebar />
+      <main className="flex-1 p-4 pb-20 md:pb-4">{children}</main>
+       <BottomNav />
     </div>
   );
 }
