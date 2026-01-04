44a0ebf517bbeac643aab6d223d4d777c27680d1
Ana Laura Flores <anadisenio@hotmail.com>
2026-01-04 17:03:24 -0300

    import use

--- src/components/CuentaCorrienteModal.jsx
+++ src/components/CuentaCorrienteModal.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useMemo, useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import CuentaCorrienteMovimientos from "./CuentaCorrienteMovimientos";
 import PagoForm from "./pagos/PagoForm";
